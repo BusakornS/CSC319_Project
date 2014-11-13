9e5d3bb3b107a8d7d2dfdc47d69e68a914a683b2
BusakornS <ploy1309@hotmail.com>
2014-11-13 12:45:03 +0700

    Submit Main Class of SpreadingFire

--- SpreadingFire/src/spreadingfire/SpreadingFire.java
+++ SpreadingFire/src/spreadingfire/SpreadingFire.java
@@ -4,16 +4,52 @@
  */
 package spreadingfire;
 
+import java.util.Scanner;
+import javax.swing.JFrame;
+
 /**
  *
  * @author NuPLoY
  */
 public class SpreadingFire {
+    static int width, height;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TUI();
+    }
+    
+    public static void TUI() {
+        System.out.println("Set the size of the forest (grid) : ");
+        System.out.print("Width = ");
+        Scanner w = new Scanner(System.in);
+        int width = w.nextInt();
+        System.out.print("Height = ");
+        Scanner h = new Scanner(System.in);
+        int height = h.nextInt();
+        
+        System.out.println("Set the probability of a tree in a cell catching fire (0 - 100) : ");
+        Scanner probFireTree = new Scanner(System.in);
+        int probC = probFireTree.nextInt();
+        
+        // System.out.println("Set the density of forest : (0 - 1) ");
+        // Scanner density = new Scanner(System.in);
+        // int probT = density.nextInt();
+        
+        Forest forest = new Forest(width, height, probC, 0, 100);
+        
+        System.out.println("Initial grid");
+        forest.printForest();
+        int step = 1;
+        while (!forest.alreadyBurn()) {
+            forest.spreading();
+            System.out.println("Step = " + step);
+            forest.printForest();
+            step++;
+            forest.createForest();
+        }
+        System.out.println("Total step = " + (step - 1));
     }
 }
